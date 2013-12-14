36f9c69fae1de2213596d58958332dc3f0655e3f
Peihui <shaopeihui1@gmail.com>
2013-12-14 14:27:50 -0500

    add sound effects; player can move any where in the screen

--- bullets_my.py
+++ bullets_my.py
@@ -15,6 +15,9 @@ bulletHeight=10
 
 blockCount=0
 blockAddRate=6
+bulletCount=0
+bulletAddRate=3
+#bulletAddRate=200
 
 screenSpeed=3
 bulletSpeed=5
@@ -81,8 +84,8 @@ class Player(pygame.sprite.Sprite):
         # as a list of two numbers.
         pos = pygame.mouse.get_pos()
     
-        # Set the player x position to the mouse x position
-        self.rect.x = pos[0] 
+        self.rect.x = pos[0]-self.rect.width//2
+        self.rect.y = pos[1]- self.rect.height//2
         
 # This class represents the bullet        
 class Bullet(pygame.sprite.Sprite):    
@@ -97,13 +100,23 @@ class Bullet(pygame.sprite.Sprite):
     def update(self):
         self.rect.y -= bulletSpeed
 
+'''fontList=pygame.font.get_fonts()
+for font in fontList:
+    print(font)
+'''    
         
 pygame.init()
 screen = pygame.display.set_mode([width,height])
+pygame.display.set_caption('Raiden Simulation Game Engine')
 
 # game resources setup
 font = pygame.font.SysFont(None, 48)
+#scoreFont=pygame.font.SysFont(["Arial","Tahoma","Times New Roman","Verdana"], 20, True, True)  #how the list should be formatted.
+scoreFont=pygame.font.SysFont("arial,tahoma", 20, True, True)
+#scoreFont=pygame.font.SysFont("arial", 20, True, True)
 gameOverSound = pygame.mixer.Sound('gameover.wav')
+shootSound=pygame.mixer.Sound('basic_shoot.wav')
+hitSound=pygame.mixer.Sound('basic_hit.wav')
 pygame.mixer.music.load('background.mid')  
 
 
@@ -137,7 +150,7 @@ topScore=0
 player.rect.y = height-playerHeight-10
 
 drawText('RAIDEN', font, screen, (width / 3), (height / 3))
-drawText('Press any key to start...', font, screen, (width / 3) - 30, (height / 3) + 50)
+drawText('Press any key to start...', font, screen, (width / 3) - 80, (height / 3) + 50)
 pygame.display.update()
 waitForPlayerToPressKey()
 
@@ -145,6 +158,7 @@ waitForPlayerToPressKey()
 while True:    
 
     pygame.mixer.music.play(-1, 0.0)
+    boolFire=False
     while True:  #game loop when the game is playing.
         #pygame.mixer.music.play(-1, 0.0)
         # --- Event Processing
@@ -155,15 +169,25 @@ while True:
                 if event.key == K_ESCAPE:
                         end()     
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                # Fire a bullet if the user clicks the mouse button
-                bullet = Bullet(bulletWidth, bulletHeight)
-                # Set the bullet so it is where the player is
-                bullet.rect.x = player.rect.x+player.rect.width//2
-                bullet.rect.y = player.rect.y
-                # Add the bullet to the lists
-                all_sprites_list.add(bullet)
-                bullet_list.add(bullet)
-    
+                boolFire=True
+            elif event.type == pygame.MOUSEBUTTONUP:
+                boolFire=False
+        
+        if boolFire:
+            bulletCount+=1
+        if boolFire and bulletCount>=bulletAddRate:
+            bulletCount=0
+            #sound for shooting
+            #shootSound.play()
+            bullet = Bullet(bulletWidth, bulletHeight)
+            # Set the bullet so it is where the player is
+            bullet.rect.x = player.rect.x+player.rect.width//2-bullet.rect.width//2
+            bullet.rect.y = player.rect.y-12
+            # Add the bullet to the lists
+            all_sprites_list.add(bullet)
+            bullet_list.add(bullet)       
+            
+             
         # --- Game logic
         
         #add blocks
@@ -191,9 +215,10 @@ while True:
             
             # For each block hit, remove the bullet and add to the score
             for block in block_hit_list:
+                hitSound.play()   
                 bullet_list.remove(bullet)
-                all_sprites_list.remove(bullet)
-                score += 1
+                all_sprites_list.remove(bullet)                             
+                score += 3
                 
             # Remove the bullet if it flies up off the screen
             if bullet.rect.y < -bulletHeight:
@@ -203,13 +228,13 @@ while True:
         # --- Draw a frame
     
         # Clear the screen
-        screen.fill(black)
-        
-        drawText('Score: %s' % (score), font, screen, 10, 0)
-        drawText('Top Score: %s' % (topScore), font, screen, 10, 40)
+        screen.fill(black)      
             
         # Draw all the spites
         all_sprites_list.draw(screen)
+        
+        drawText('Score: %s' % (score), scoreFont, screen, 10, 0)
+        drawText('Top Score: %s' % (topScore), scoreFont, screen, 10, 20)
     
         # Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
